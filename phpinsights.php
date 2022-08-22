391728af9ebd86f9627f162f2a2e9bdeee8e71b2
iamsaltedfish <90859800+iamsaltedfish@users.noreply.github.com>
2022-08-22 12:41:03 +0800

    set phpinsights

--- phpinsights.php
+++ phpinsights.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 return [
     'preset' => 'default',
     'remove' => [
-        PhpCsFixer\Fixer\FunctionNotation\VoidReturnFixer::class,
+        SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff::class,
+        PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff::class,
+        SlevomatCodingStandard\Sniffs\Functions\FunctionLengthSniff::class,
+        SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff::class,
+        SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff::class,
+        NunoMaduro\PhpInsights\Domain\Insights\ForbiddenGlobals::class,
+    ],
+    'config' => [
     ],
     'exclude' => [
         'app/routes.php',

c7f14bf22bbf719d2b43311a86a5076f2a55817a
iamsaltedfish <90859800+iamsaltedfish@users.noreply.github.com>
2023-01-20 22:35:36 +0800

    format code style

--- src/Controllers/Admin/SubscribeLogController.php
+++ src/Controllers/Admin/SubscribeLogController.php
@@ -1,21 +1,17 @@
 <?php
 namespace App\Controllers\Admin;
 
-use Slim\Http\{
-    Request,
-    Response
-};
-use App\Utils\Tools;
-use App\Models\UserSubscribeLog;
 use App\Controllers\AdminController;
+use App\Models\UserSubscribeLog;
+use App\Utils\Tools;
 
 class SubscribeLogController extends AdminController
 {
     public function index($request, $response, $args)
     {
         $logs = UserSubscribeLog::orderBy('id', 'desc')
-        ->limit($_ENV['page_load_data_entry'])
-        ->get();
+            ->limit($_ENV['page_load_data_entry'])
+            ->get();
 
         return $response->write(
             $this->view()
@@ -27,8 +23,8 @@ class SubscribeLogController extends AdminController
 
     public function subscribe_ajax($request, $response, $args)
     {
-        $email    = $request->getParam('email');
-        $user_id  = $request->getParam('user_id');
+        $email = $request->getParam('email');
+        $user_id = $request->getParam('user_id');
         $user_name = $request->getParam('user_name');
         $request_ip = $request->getParam('request_ip');
         $subscribe_type = $request->getParam('subscribe_type');
@@ -41,21 +37,20 @@ class SubscribeLogController extends AdminController
         ($user_name != '') && array_push($condition, ['user_name', '=', $user_name]);
         ($request_ip != '') && array_push($condition, ['request_ip', '=', $request_ip]);
         ($subscribe_type != '') && array_push($condition, ['subscribe_type', '=', $subscribe_type]);
-        ($request_user_agent != '') && array_push($condition, ['request_user_agent', 'like', '%'.$request_user_agent.'%']);
+        ($request_user_agent != '') && array_push($condition, ['request_user_agent', 'like', '%' . $request_user_agent . '%']);
 
         $results = UserSubscribeLog::orderBy('id', 'desc')
-        ->where($condition)
-        ->limit($_ENV['page_load_data_entry'])
-        ->get();
+            ->where($condition)
+            ->limit($_ENV['page_load_data_entry'])
+            ->get();
 
-        foreach($results as $result)
-        {
+        foreach ($results as $result) {
             $result->ip_info = Tools::getIpInfo($result->request_ip);
         }
 
         return $response->withJson([
             'ret' => 1,
-            'result' => $results
+            'result' => $results,
         ]);
     }
 }

f55d6d95688a2f4389fc2bc994de8164d94ddcf4
iamsaltedfish <90859800+iamsaltedfish@users.noreply.github.com>
2023-01-29 18:00:10 +0800

    rollback ProductOrder.php

--- src/Models/ProductOrder.php
+++ src/Models/ProductOrder.php
@@ -10,18 +10,20 @@ class ProductOrder extends Model
     public static function judgmentOrderStatus($status, $order_expired_at, $style = false)
     {
         if ($status === 'paid') {
-            return $style ? '<span class="status status-green">完成</span>' : '完成';
+            return ($style) ? '<span class="status status-green">完成</span>' : '完成';
         }
         if ($status === 'refunded') {
-            return $style ? '<span class="status status-purple">已退</span>' : '已退';
+            return ($style) ? '<span class="status status-purple">已退</span>' : '已退';
         }
         if ($status !== 'abnormal') {
             if (time() > $order_expired_at) {
-                return $style ? '<span class="status status-black">超时</span>' : '超时';
+                return ($style) ? '<span class="status status-black">超时</span>' : '超时';
+            } else {
+                return ($style) ? '<span class="status status-orange">等待</span>' : '等待';
             }
-            return $style ? '<span class="status status-orange">等待</span>' : '等待';
+        } else {
+            return ($style) ? '<span class="status status-red">异常</span>' : '异常';
         }
-        return $style ? '<span class="status status-red">异常</span>' : '异常';
     }
 
     public static function translateOrderStatus(string $status, int $order_expired_at): string
@@ -35,9 +37,11 @@ class ProductOrder extends Model
         if ($status !== 'abnormal') {
             if (time() > $order_expired_at) {
                 return '超时';
+            } else {
+                return '等待支付';
             }
-            return '等待支付';
+        } else {
+            return '异常';
         }
-        return '异常';
     }
 }
